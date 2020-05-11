292c5dd73b65a949feda8eed7ae85080dc1042f5
Tom Trabue <tom.trabue@gmail.com>
2020-05-11 16:35:02 -0400

    Added more solutions for chapter 7

--- programming_in_haskell/ch7_higher_order_functions/Answers.hs
+++ programming_in_haskell/ch7_higher_order_functions/Answers.hs
@@ -27,3 +27,45 @@ myAll p = foldl (\b a -> b && p a) True
 -- b.
 myAny :: (a -> Bool) -> [a] -> Bool
 myAny p = foldl (\b a -> b || p a) True
+-- c.
+myTakeWhile :: (a -> Bool) -> [a] -> [a]
+myTakeWhile p (x : xs) | null xs || not (p x) = []
+                       | otherwise            = x : myTakeWhile p xs
+-- d.
+myDropWhile :: (a -> Bool) -> [a] -> [a]
+myDropWhile p (x : xs) | null xs   = []
+                       | not (p x) = xs
+                       | otherwise = myDropWhile p xs
+
+{-
+#3 Redefine the functions map f and filter p using foldr.
+-}
+
+-- Solution:
+myMap :: (a -> b) -> [a] -> [b]
+myMap f = foldr (\a bs -> f a : bs) []
+
+myFilter :: (a -> Bool) -> [a] -> [a]
+myFilter p = foldr addIfPred []
+ where
+  addIfPred y ys | p y       = y : ys
+                 | otherwise = ys
+
+{-
+#4 Using foldl, define a function dec2int :: [Int] -> Int that converts a
+decimal number into an integer. For example,
+> dec2int [2,3,4,5]
+2345
+-}
+
+-- Solution:
+dec2int :: [Int] -> Int
+dec2int = foldl concatInt 0
+  where concatInt x m = read (show x ++ show m) :: Int
+
+{-
+#5 Without looking at the definitions from the standard prelude, define the
+higher-order library function curry that converts a function on pairs into
+a curried function, and, conversely, the function uncurry that converts a
+curried function with two arguments into a function on pairs.
+-}
