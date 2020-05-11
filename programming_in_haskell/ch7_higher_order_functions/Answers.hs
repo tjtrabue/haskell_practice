2d9faebc484d17ace86e87318dd6c3066bf9b29d
Tom Trabue <tom.trabue@gmail.com>
2020-05-10 20:15:04 -0400

    Added some answers for chapter 7

--- programming_in_haskell/ch7_higher_order_functions/Answers.hs
+++ programming_in_haskell/ch7_higher_order_functions/Answers.hs
@@ -6,3 +6,24 @@ module Ch7Answers where
 -- Solution:
 -- map_and_filter f p xs = filter p $ map f xs
 -- that is, [f x | x <- xs, p x] === filter p $ map f xs
+
+{-
+#2 Without looking at the definitions from the standard prelude, define
+the following higher-order library functions:
+a. Decide if all elements of a list satisfy a predicate:
+   all :: (a -> Bool) -> [a] -> Bool
+b. Decide if any element of a list satisfies a predicate:
+   any :: (a -> Bool) -> [a] -> Bool
+c. Select elements from a list while they satisfy a predicate
+   takeWhile :: (a -> Bool) -> [a] -> [a]
+d. Remove elements from a list while they satisfy a predicate
+   dropWhile :: (a -> Bool) -> [a] -> [a]
+-}
+
+-- Solution:
+-- a.
+myAll :: (a -> Bool) -> [a] -> Bool
+myAll p = foldl (\b a -> b && p a) True
+-- b.
+myAny :: (a -> Bool) -> [a] -> Bool
+myAny p = foldl (\b a -> b || p a) True
