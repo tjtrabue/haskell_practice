c177d59bead027cbe20edd21fd6207844737c315
Tom Trabue <tom.trabue@gmail.com>
2020-05-24 14:55:16 -0400

    Adding new changes

--- programming_in_haskell/ch8_declaring_types_and_classes/Answers.hs
+++ programming_in_haskell/ch8_declaring_types_and_classes/Answers.hs
@@ -3,6 +3,7 @@
 module Answers where
 
 -- A natural number type
+import           Data.List
 data Nat = Zero | Succ Nat
 
 -- Turn a natural number into an Int
@@ -12,21 +13,27 @@ evalNat (Succ n) = 1 + evalNat n
 
 -- Add two natural numbers, producing another
 addNat :: Nat -> Nat -> Nat
-addNat Zero n     = n
+addNat Zero     n = n
 addNat (Succ m) n = Succ (addNat m n)
 
 n :: Nat
-n = Succ(Succ(Succ(Succ Zero)))
+n = Succ (Succ (Succ (Succ Zero)))
 
 m :: Nat
-m = Succ(Succ(Succ Zero))
+m = Succ (Succ (Succ Zero))
 
 -- NOTE: You cannot put a type constraint on a data type declaration in Haskell.
 --       We must declare a Tree with an Ord parameter type, such as Int.
-data Tree a = Leaf a | Node (Tree a) a (Tree a)
+data Tree a = Leaf a | Node (Maybe (Tree a)) a (Maybe (Tree a))
+  deriving (Show)
 
 t :: Tree Int
-t = Node (Node (Leaf 1) 3 (Leaf 4)) 5 (Node (Leaf 6) 7 (Leaf 9))
+-- t = Node ((Node (Leaf 1)) 3 (Leaf 4)) 5 (Node (Leaf 6) 7 (Leaf 9))
+t = Node (Just (Node (Just (Leaf 1)) 3 (Just (Leaf 4))))
+         5
+         (Just (Node (Just (Leaf 6)) 7 (Just (Leaf 9))))
+
+data Expr = Val Int | Add Expr Expr
 
 {-
 #1 In a similar manner to the function add,a define a recursive multiplication
@@ -36,15 +43,15 @@ Hint: make use of add in your definition.
 mult :: Nat -> Nat -> Nat
 mult _ Zero        = Zero
 mult m (Succ Zero) = m
-mult m (Succ n)    = addNat m (mult m n)
+mult m (Succ n   ) = addNat m (mult m n)
 
 {-
 #2
 -}
-occurs :: Ord a => a -> Tree a -> Bool
-occurs x (Leaf y)     = x == y
--- occurs x (Node l y r) = x == y || occurs x l || occurs x r
-occurs x (Node l y r) = case compare x y of
+occurs :: Ord a => a -> Maybe (Tree a) -> Bool
+occurs _ Nothing                       = False
+occurs x (Just (Leaf y              )) = x == y
+occurs x (Just (Node l y r)) = case compare x y of
   EQ -> True
   LT -> occurs x l
   GT -> occurs x r
@@ -56,14 +63,41 @@ occurs x (Node l y r) = case compare x y of
 {-
 #3
 -}
+descendAndCountLeaves :: Maybe (Tree a) -> Integer
+descendAndCountLeaves Nothing             = 0
+descendAndCountLeaves (Just (Leaf _    )) = 1
+descendAndCountLeaves (Just (Node l _ r)) =
+  descendAndCountLeaves l + descendAndCountLeaves r
+
+numLeaves :: Tree a -> Integer
+numLeaves x = descendAndCountLeaves (Just x)
+
+balanced :: Tree a -> Bool
+balanced (Leaf _    ) = True
+balanced (Node l _ r) =
+  abs (descendAndCountLeaves l - descendAndCountLeaves r) <= 1
 
 {-
 #4
 -}
 
+splitList :: [a] -> ([a], [a])
+splitList xs = splitAt (length xs `div` 2) xs
+
+balance :: Ord a => [a] -> Maybe (Tree a)
+balance []  = Nothing
+balance [x] = Just (Leaf x)
+balance xs  = Just (Node (balance first) m (balance second))
+  where (first, (m : second)) = splitList . sort $ xs
+
 {-
 #5
 -}
+-- We can think of f as a transformer function on each value of the expression,
+-- and g as a binary operator for the transformed values.
+folde :: (Int -> a) -> (a -> a -> a) -> Expr -> a
+folde f _ (Val i)     = f i
+folde f g (Add e1 e2) = folde f g e1 `g` folde f g e2
 
 {-
 #6
